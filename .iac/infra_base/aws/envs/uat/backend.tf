f6a14fb509ac9dff30d70fa66fd771f14644db62
Carlos Soriano <soriano.carlitos@gmail.com>
2023-02-22 16:00:07 -0500

    bump tf versions and add main for s3_object

--- .iac/infra_base/aws/envs/uat/backend.tf
+++ .iac/infra_base/aws/envs/uat/backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.13.5"
+  required_version = ">= 1.3.0"
   backend "s3" {
     region         = "us-east-1"
     bucket         = "341864192726-terraform-backend"
